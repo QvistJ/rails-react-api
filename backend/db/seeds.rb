172ff71b5d92a88ec20d1637c3529ea99daff7e5
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-09-03 20:32:48 +0200

    Basic backend api setup

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(title: 'Setup api', description: 'This task is really big, since its everything from rails to react!', status: 'TODO', points: 100)
