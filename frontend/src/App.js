34ce8f76bb9c47a27f34584e56f15e5f693fb459
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-09-05 20:28:37 +0200

    Setup basic react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Tasks from './Tasks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render (){
+    return (
+      <div>
+        <nav>
+          <a href="/show">Show</a>
+          <a href="/create">Create</a>
+        </nav>
+        <Tasks />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
