aa4812536347d0d9296886c0baad62d5c8f7e2c3
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-09-11 20:45:06 +0200

    Created a nav :), after npm making my PC bluescreen.............

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import Users from './Users'
+import Contact from './Contact'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} />
+      <Route path="/users" component={Users} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'))
